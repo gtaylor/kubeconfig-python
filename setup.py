7f89525cb627a6883db9d76bd2f884a3ca6125cb
Greg Taylor <gtaylor@gc-taylor.com>
2017-08-11 22:21:52 -0700

    Bumping version to 1.0 in prep for release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kubeconfig',
-    version='0.1.0',
+    version='1.0.0',
     packages=find_packages(),
     install_requires=[
         'PyYAML',
