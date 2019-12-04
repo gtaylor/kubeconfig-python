c806470c2e8c4390a7f21a84320289d250dd841f
Greg Taylor <greg.taylor@reddit.com>
2019-12-03 16:28:58 -0800

    Require PyYAML>=5.2

--- setup.py
+++ setup.py
@@ -9,10 +9,10 @@ setup(
     author_email='greg@gctaylor.com',
     license='BSD',
     url='http://kubeconfig-python.readthedocs.io',
-    version='1.1.0',
+    version='1.1.1',
     packages=find_packages(),
     install_requires=[
-        'PyYAML',
+        'PyYAML>=5.2',
     ],
     tests_require=[
         'mock',
